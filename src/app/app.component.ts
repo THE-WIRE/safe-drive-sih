69ab0b5fa67a1fd8d31fec57c2bca4ce37bc1ad9
Suyog <itssuyog96@gmail.com>
2018-03-27 14:12:10 +0530

    basic login/register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,61 @@
-import { Component } from '@angular/core';
-import { Platform, AlertController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, AlertController, NavController, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireAuth } from "angularfire2/auth";
+
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { Page } from 'ionic-angular/navigation/nav-util';
+import { RegisterPage } from '../pages/register/register';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:Page = HomePage;
+  loginPage:Page = LoginPage;
+  registerPage:Page = RegisterPage
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private alertCtrl: AlertController) {
+  isAuthenticatd: boolean = false;
+
+  @ViewChild('nav') nav : NavController;
+
+  constructor(
+    platform: Platform, 
+    statusBar: StatusBar, 
+    splashScreen: SplashScreen, 
+    private alertCtrl: AlertController, 
+    private menuCtrl: MenuController,
+    private af:AngularFireAuth) {
+
+    af.authState.subscribe(user => {
+      if(user){
+        //Logged in
+        this.isAuthenticatd = true;
+        this.rootPage = HomePage
+      } else {
+        //Redirect to login page
+        this.isAuthenticatd = false;
+        this.rootPage = LoginPage
+      }
+    })
+
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
-
-      alertCtrl.create({
-        title: 'Kuch hua',
-        subTitle: 'Jo hua uska details',
-        buttons: [{
-          text: 'Ok',
-          handler: event => {
-            console.log(event);
-          }
-        }, 'Cancel']
-      }).present();
-
     });
+  }
+
+  onLoad(page: Page){
+    this.nav.setRoot(page);
+    this.menuCtrl.close();
+  }
+
+  onLogOut(){
+    this.af.auth.signOut();
+    this.menuCtrl.close();
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,46 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule, AngularFireAuth } from "angularfire2/auth";
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
+
+const environment ={
+  production:false,
+  firebase:{
+    apiKey: "AIzaSyBXtKT8C2d53fKzizznKFefOVqC5M46mSw",
+    authDomain: "sih-2-3e356.firebaseapp.com",
+    databaseURL: "https://sih-2-3e356.firebaseio.com",
+    projectId: "sih-2-3e356",
+    storageBucket: "sih-2-3e356.appspot.com",
+    messagingSenderId: "693586778503"
+  }
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    RegisterPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    RegisterPage
   ],
   providers: [
     StatusBar,
