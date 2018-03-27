8c29cc1dc6fb01cc3760d36704f2863eef1239f3
Suyog <itssuyog96@gmail.com>
2018-03-27 22:32:58 +0530

    register done

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
+import { AngularFireAuth } from "angularfire2/auth";
+import { NgForm } from "@angular/forms";
+import { LoginPage } from '../login/login';
 
-/**
- * Generated class for the RegisterPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import firebase from 'firebase';
 
 @IonicPage()
 @Component({
@@ -15,11 +13,49 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RegisterPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  loginPage:any = LoginPage;
+
+  cities: String[] = [
+    'Mumbai', 'Delhi', 'Kolkata', 'Kashmir', 'Karachi'
+  ]
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    private alertCtrl: AlertController,
+    private loadCtrl: LoadingController,
+    private af:AngularFireAuth) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
   }
 
+  onSignUp(data){
+    const loader = this.loadCtrl.create({
+      content: 'Creating your account. Please Wait...'
+    });
+    loader.present();
+
+    this.af.auth.createUserWithEmailAndPassword(data.email, data.password)
+    .then(success => {
+      //Entry in Database
+      loader.dismiss();
+      data.password = null;
+      data['role'] = 0;
+      firebase.database().ref('users/'+success.uid).set(data)
+    })
+    .catch(error => {
+      loader.dismiss();
+      this.alertCtrl.create({
+        title: 'Sign Up Error',
+        subTitle: error.message,
+        buttons: ['OK']
+      }).present();
+    });
+  }
+
+  onLoad(page:any){
+    this.navCtrl.setRoot(page);
+  }
 }
