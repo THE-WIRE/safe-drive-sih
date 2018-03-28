c934624b8f16c2b05e0ef498a571433a38a803c4
Suyog <itssuyog96@gmail.com>
2018-03-28 20:11:31 +0530

    safe drive mode

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,87 @@
 import { Component } from '@angular/core';
-import { NavController, FabContainer, AlertController, ModalController } from 'ionic-angular';
+import { NavController, FabContainer, AlertController, ModalController, LoadingController } from 'ionic-angular';
 import {AddEntry} from '../../components/add-entry/add-entry'
 import { AddIssuePage } from '../add-issue/add-issue';
+import { Geolocation } from '@ionic-native/geolocation';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController,
-              private alertCtrl:AlertController,
-              private modalCtrl:ModalController) {
 
+  safe_drive_modes: any = [
+    {
+      text: 'SAFE DRIVE MODE OFF',
+      color: 'danger'
+    },
+    {
+      text: 'SAFE DRIVE MODE ON',
+      color: 'secondary'
+    },
+    {
+      text: 'CALL BLOCKING MODE OFF',
+      color: 'danger'
+    }
+  ]
+
+  safe_drive_btn: any = this.safe_drive_modes[0];
+  watcher:any = null;
+  options: any = {
+    // timeout: 30000,
+    enableHighAccuracy: true
+  }
+  loader:any;
+  speed: any;
+
+  constructor(
+    public navCtrl: NavController,
+    private alertCtrl:AlertController,
+    private geoloc: Geolocation,
+    private loadCtrl: LoadingController,
+    private modalCtrl:ModalController) {
+
+    this.safe_drive_btn = this.safe_drive_modes[0];
+    this.loader = loadCtrl.create({
+      content: 'Starting Safe Drive Mode...'
+    });
+  }
+
+  onSafeDriveClick(event){
+    if(this.safe_drive_btn.color == 'danger'){
+      this.loader.present();
+      this.startSafeDrive();
+
+    } else if(this.safe_drive_btn.color == 'secondary' || this.safe_drive_btn.color == 'default'){
+      this.stopSafeDrive();
+      this.safe_drive_btn = this.safe_drive_modes[0];
+    }
+  }
+
+  startSafeDrive(){
+    this.watcher = this.geoloc.watchPosition(this.options).subscribe(loc => {
+      if(loc != undefined && !loc['code']){
+        if(loc.coords.speed != undefined){
+          console.log(loc.coords.speed);
+          this.speed = loc.coords.speed;
+          this.loader.dismiss();
+          this.safe_drive_btn = this.safe_drive_modes[1];
+        }
+      }
+      if(loc['code']){
+        this.loader.dismiss();
+        this.alertCtrl.create({
+          title: 'Error starting drive mode!',
+          subTitle: 'Unable to detect vehicle speed',
+          buttons: ['OK']
+        })
+      }
+    });
+  }
+
+  stopSafeDrive(){
+    if(this.watcher)
+      this.watcher.unsubscribe();
   }
 
   addAccProne(addto:string,fab:FabContainer){
