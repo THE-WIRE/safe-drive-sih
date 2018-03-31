532d305e8c957299963999a47b8196f14bb79f63
Abhishri <abhishripawar@gmail.com>
2018-03-31 09:29:24 +0530

    Main button added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,19 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { ProgressBarComponent } from '../components/progress-bar/progress-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ProgressBarComponent
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ProgressBarComponent } from '../components/progress-bar/progress-bar';
 
 @Component({
   selector: 'page-home',
@@ -11,13 +12,57 @@ export class HomePage {
 
   }
 
-myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.innerHTML === "Drive Mode ON") {
-        x.innerHTML = "Drive Mode OFF";
-    } else {
-        x.innerHTML = "Drive Mode ON";
+  // progressbar.js@1.0.0 version is used
+  // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+
+  myFunction() {
+      var x = document.getElementById("myDIV");
+      if (x.innerHTML === "Drive Mode OFF") {
+          x.innerHTML = "Drive Mode ON";
+      var ProgressBar = require('progressbar.js')
+      var bar = new ProgressBar.Circle(container, {
+      color: '#aaa',
+    // This has to be the same size as the maximum width to
+    // prevent clipping
+      strokeWidth: 3,
+      trailWidth: 2,
+      easing: 'easeInOut',
+      duration: 5400,
+      text: {
+     autoStyleContainer: 'false'
+    },
+    from: { color: '#aaa', width: 1 },
+    to: { color: '#222', width: 4.3 },
+    // Set default step function for all animate calls
+    step: function(state, circle) {
+      circle.path.setAttribute('stroke', state.color);
+      circle.path.setAttribute('stroke-width', state.width);
+
+      var value = Math.round(circle.value() * 20);
+      //console.log(value);
+
+      if (value === 0) {
+        circle.setText('');
+      }
+      else if(value === 20){
+
+            circle.path.setAttribute('stroke', 'green');
+            //console.log(circle);
+      }
+
+
     }
+  });
+
+  bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+  bar.text.style.fontSize = '4rem';
+
+  bar.animate(1.0);  // Number from 0.0 to 1.0
+
 }
 
+else {
+    x.innerHTML = "Drive Mode OFF";
+}
+}
 }
